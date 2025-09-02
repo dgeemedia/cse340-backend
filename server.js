dedf973f05ae52ee7d56d0ff0b3cea3a0034897b
dgeemedia <lolumah@byupathway.edu>
2025-09-03 00:40:29 +0100

    getting build successful but render showing cannot get /

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get("/", function (req, res) {
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT || 5500
+const port = process.env.PORT
 const host = process.env.HOST
 
 /* ***********************
